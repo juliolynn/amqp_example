dbc2d8fabc2df0750683d2abc3801fe3e436b6b4
Julio Lynn <julio.lynn@gmail.com>
2022-06-17 19:03:59 -0700

    Created a consumer and added queue declaration

--- amqp_consumer.py
+++ amqp_consumer.py
@@ -19,9 +19,18 @@ parameters = pika.URLParameters(url)
 parameters.ssl_options = pika.SSLOptions(context=ssl_context)
 
 connection = pika.BlockingConnection(parameters)
-
 channel = connection.channel()
-channel.queue_declare(queue='test')
-channel.basic_publish(exchange='', routing_key='test',
-                      body=b'Test message.')
+
+for method_frame, properties, body in channel.consume('test'):
+    # Display the message parts and acknowledge the message
+    print(method_frame, properties, body)
+    channel.basic_ack(method_frame.delivery_tag)
+
+    # Escape out of the loop after 10 messages
+    if method_frame.delivery_tag == 10:
+        break
+
+# Cancel the consumer and return any pending messages
+requeued_messages = channel.cancel()
+print('Requeued %i messages' % requeued_messages)
 connection.close()
